d9ec7bd3542a11c5ff01596fb8db9af7c5db1fd6
Mathieu Desjarlais <github@desmat.ca>
2024-12-02 14:51:00 -0500

    .find and .ids: support for many lookups

--- src/example.ts
+++ src/example.ts
@@ -3,7 +3,13 @@ require('dotenv').config();
 
 type User = {
   id: string,        // required; set by .create as short UUID by default, or can be provided
-  createdAt: number, // required; set by .create
+  createdAt: number, // required; set by .create as tineinmillis
+  name: string,
+};
+
+type Category = {
+  id: string,
+  createdAt: number,
   name: string,
 };
 
@@ -11,24 +17,54 @@ type Thing = {
   id: string,
   createdAt: number,
   createdBy: string, // needed to lookup Things by Users
+  category: string   // needed to lookup Things by Category
   label: string,
 };
 
+
 const ThingOptions = {
   lookups: {
-    user: "createdBy", // enable .find({ user: <USERID> })
+    user: "createdBy",    // enable .find({ user: <USERID> })
+    category: "category", // enable .find({ category: <CATEGORYID> })
   },
 };
 
 // with environment variables KV_REST_API_URL and KV_REST_API_TOKEN
 // or `url` and `token` keys provided to RedisStore constructor
-
+// const debug = true;
 const store = {
-  users: new RedisStore<User>({ key: "user" }),
-  things: new RedisStore<Thing>({ key: "thing", options: ThingOptions }),
+  users: new RedisStore<User>({ key: "example-user", debug }),
+  categories: new RedisStore<Category>({ key: "example-category", setKey: "example-categories", debug }),
+  things: new RedisStore<Thing>({ key: "example-thing", options: ThingOptions, debug }),
 };
 
+async function cleanup() {
+  // cleanup all from previous session
+  let [
+    users,
+    categories,
+    things,
+  ] = await Promise.all([
+    store.users.ids({ scan: "*" }),
+    store.categories.ids({ scan: "*" }),
+    store.things.ids({ scan: "*" }),
+  ])
+  // console.log("users to delete", { usersToDelete, postsToDelete, userPostsToDelete });
+  return Promise.all([
+    // @ts-ignore
+    ...Array.from(users).map((id: string) => store.users.delete(id, { hardDelete: true })),
+    // @ts-ignore
+    ...Array.from(categories).map((id: string) => store.categories.delete(id, { hardDelete: true })),
+    // @ts-ignore
+    ...Array.from(things).map((id: string) => store.things.delete(id, { hardDelete: true })),
+  ]);
+}
+
 (async function () {
+  
+  // fully cleanup previous session  
+  // await cleanup();
+
 
   // create users
 
@@ -50,30 +86,41 @@ const store = {
   // ]
 
 
-  // create things
+  // create categories and things
+
+  const categories = await Promise.all([
+    store.categories.create({ id: "category1", name: "Category One" }),
+    store.categories.create({ id: "category2", name: "Category Two" }),
+  ]);
 
   const things = await Promise.all([
     store.things.create({
       createdBy: users[0].id,
+      category: categories[0].id,
       label: "A thing for user one",
     }),
     store.things.create({
       createdBy: users[0].id,
+      category: categories[0].id,
       label: "Another thing for user one",
     }),
     store.things.create({
       createdBy: users[0].id,
+      category: categories[1].id,
       label: "Yet another thing for user one",
     }),
     store.things.create({
       createdBy: users[1].id,
+      category: categories[1].id,
       label: "A thing for user two",
     }),
   ]);
 
-  // JSON.SET thing:<UUID> $ '{ "id": "<UUID>", "createdAt": <TIMEINMILLIS>, "createdBy": "<USER_UUID>", "message": "Another thing for user one" }'
+  // Adding additional sets for lookup:
+  // JSON.SET thing:<UUID> $ '{ "id": "<UUID>", "createdAt": <TIMEINMILLIS>, "createdBy": "<USER_UUID>", "categoryId": "<CATEGORY_UUID>", label": "Another thing for user one" }'
   // ZADD things <TIMEINMILLIS> <UUID>
   // ZADD things:user:<USER_UUID> <TIMEINMILLIS> <UUID>
+  // ZADD things:category:<CATEGORY_UUID> <TIMEINMILLIS> <UUID>
   // ... 
 
   console.log("things", things);
@@ -83,6 +130,7 @@ const store = {
   //     id: '<UUID>',
   //     createdAt: <TIMEINMILLIS>,
   //     createdBy: '<USER_UUID>',
+  //     category: `<CATEGORY_UUID>',
   //     label: 'A thing for user one'
   //   },
   //   ...
@@ -93,7 +141,8 @@ const store = {
 
   const allThings = await store.things.find();
 
-  // ZRANGE	things:user:<USER_UUID> 0 -1 REV
+  // Get thing ids from set of all things then pull their values:
+  // ZRANGE	things 0 -1 REV
   // JSON.MGET thing:<THING1_UUID> thing:<THING2_UUID> ...
 
   console.log("allThings", allThings); // 4 entries
@@ -103,16 +152,35 @@ const store = {
 
   const latestUserThings = await store.things.find({ user: users[0].id });
 
+  // Get thing ids from set of user lookup things then pull their values:
   // ZRANGE	things:user:<USER_UUID> 0 -1 REV
   // JSON.MGET thing:<THING1_UUID> thing:<THING2_UUID> thing:<THING3_UUID>
 
   console.log("latestUserThings", latestUserThings); // 3 entries
 
 
+  // latest things from first user in first category
+
+  const latestUserThingsOfCategory1 = await store.things.find({
+    user: users[0].id,
+    category: categories[0].id,
+  });
+
+  // Get thing ids from both set of user lookup things 
+  // and category lookup things, calculate intersection, 
+  // then pull their values:
+  // ZRANGE	things:user:<USER_UUID> 0 -1 REV
+  // ZRANGE	things:category:<CATEGORY_UUID> 0 -1 REV
+  // JSON.MGET thing:<THING1_UUID> thing:<THING2_UUID> thing:<THING3_UUID>
+
+  console.log("latestUserThingsOfCategory1", latestUserThingsOfCategory1); // 2 entries
+
+
   // cleanup from this session (soft delete by default)
 
   await Promise.all([
     ...users.map((user: User) => store.users.delete(user.id)),
+    ...categories.map((user: User) => store.categories.delete(user.id)),
     ...things.map((thing: Thing) => store.things.delete(thing.id)),
   ]);
 
